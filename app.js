65685a0050a221535af8ec7a7819708ae584b527
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2023-08-08 09:48:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+// console.log("This is the practice of Maths Methods")
+
+// // 1 => Floor Method -> Prints the number to nearest integer
+
+// let maths = 12.1;
+// console.log(Math.floor(maths));
+
+// // 2=> Ceil Method -> Prints the number to farest integer
+
+// let maths1 = 12.1;
+// console.log(Math.ceil(maths1));
+
+// // 3=> Round Method -> Prints the number to nearest integer same as Floor method
+
+// let maths2 = 12.1;
+// console.log(Math.round(maths2));
+
+// // 4=> Random Method -> Prints a random number between 0 and 1
+
+// console.log(Math.random());
+
+// // 5=> Max Method -> Returns the largest number in an array
+
+// let prop = prompt("Enter the first number")
+// let prop1 = prompt("Enter the second number")
+// console.log("The max number is", Math.max(prop,prop1));
+
+// // 6=> Min Method -> Returns the smallest number in an array
+
+// let prop2 = prompt("Enter the first number")
+// let prop3 = prompt("Enter the second number")
+// console.log("The min number is", Math.min(prop2,prop3));
+
+// // 7=> Pow Method -> Returns the power of a number
+
+// // let prop4 = prompt("Enter the first number")
+// // let prop5 = prompt("Enter the second number")
+// // console.log("The power of the number is", Math.pow(prop4,prop5));
+// console.log(Math.pow(5,3))
+
+// // 8=> Abs Method -> Returns the absolute value of a number
+
+
+// let prop6 = prompt("Enter the number")
+// console.log("The absolute value is", Math.abs(prop6));
+
+// // 9=> Sqrt Method -> Returns the square root of a number
+
+// let prop7 = prompt("Enter the number")
+// console.log("The square root of the number is", Math.sqrt(prop7));
+
+// // 10=> Cbrt Method -> Returns the cube root of a number
+
+// let prop8 = prompt("Enter the number")
+// console.log("The cube root of the number is", Math.cbrt(prop8));    
+
+// // 11=> Pi Method -> Returns the value of pi
+
+// console.log("The value of pi is", Math.PI);
+
+// // 12=> Sin Method -> Returns the sin0 of a number
+
+// let prop9 = prompt("Enter the number")
+// console.log("The sin of the number is", Math.sin(prop9));
+
+// // 13=> Cos Method -> Returns the cos0 of a number
+
+// let prop10 = prompt("Enter the number")
+// console.log("The cos of the number is", Math.cos(prop10));
+
+// // 14=> Tan Method -> Returns the tan0 of a number
+
+// let prop11 = prompt("Enter the number")
+// console.log("The tan of the number is", Math.tan(prop11));
+
+// // 15=> Log Method -> Returns the log
+
+// let prop12 = prompt("Enter the number")
+// console.log("The log of the number is", Math.log(prop12));
+
+
+
+
